2714516978322a0ee6ee048518f38aa0c0644dc9
guthemberg.alves <guthemberg.alves@crptecnologia.com.br>
2023-11-03 09:15:54 -0300

    primeiro commit.

---
+++ dataethics/src/main/java/br/unitins/dataethics/DataethicsApplication.java
@@ -0,0 +1,13 @@
+package br.unitins.dataethics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataethicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataethicsApplication.class, args);
+	}
+
+}
