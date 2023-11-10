090478554bc8fb80fda644829d951b9bb6ffa5f7
guthemberg alves <guthorodrigues2009@gmail.com>
2023-11-10 16:13:34 -0300

    Feedback configuração inicial.

---
+++ dataethics/src/main/java/br/unitins/dataethics/data/Feedback.java
@@ -0,0 +1,25 @@
+package br.unitins.dataethics.data;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Feedback  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String mensagem;
+    private String email;
+    private Boolean isIdentificado;
+    private String categoria;
+}
