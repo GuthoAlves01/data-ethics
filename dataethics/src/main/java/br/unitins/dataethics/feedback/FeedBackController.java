f53204a8fa58e06c7e04c223beb4823fd3b4bba0
guthemberg.alves <guthorodrigues2009@gmail.com>
2023-11-13 20:41:56 -0300

    Controller adicionando urls para as views

--- dataethics/src/main/java/br/unitins/dataethics/feedback/FeedBackController.java
+++ dataethics/src/main/java/br/unitins/dataethics/feedback/FeedBackController.java
@@ -2,7 +2,6 @@ package br.unitins.dataethics.feedback;
 
 import br.unitins.dataethics.entity.Feedback;
 import br.unitins.dataethics.entity.FeedbackRepository;
-import org.jeasy.random.EasyRandom;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,16 +12,59 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Controller
 public class FeedBackController {
     @Autowired
     private FeedbackRepository repository;
+//
+//    index / e /index
+//    formulario /formulario
+    //    botão salvar /formulario/add
+//    quemsomos /quemsomos
+//    codejava /codejava
+//    listar - autenticado /auth/listar
+
+    @GetMapping("/")
+    @ResponseBody
+    public RedirectView barraRedirectToIndex(Model model) {
+        return new RedirectView("/index");
+    }
+
+    @GetMapping("/index")
+    @ResponseBody
+    public ModelAndView index(Model model) {
+//        model.addAttribute("feedbacks", repository.findAll());
+        return new ModelAndView("index");
+    }
+
+    @GetMapping("/feedback")
+    @ResponseBody
+    public ModelAndView requestForm(Feedback feedback, Model model) {
+        model.addAttribute("feedback", feedback);
+        return new ModelAndView("formulariocontato");
+    }
+
+    @PostMapping("/feedback/add")
+    @ResponseBody
+    public RedirectView inserirFeedback(Feedback feedback, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return new RedirectView("formulariocontato");
+        }
+        repository.save(feedback);
+
+        return new RedirectView("/index");
+    }
+    @GetMapping("/quem-somos")
+    @ResponseBody
+    public ModelAndView quemSomos() {
+        return new ModelAndView("quemsomos");
+    }
+
+    @GetMapping("/code-java")
+    @ResponseBody
+    public ModelAndView codeJava() {
+        return new ModelAndView("codejava");
+    }
 
     @GetMapping("/list")
     @ResponseBody
@@ -31,21 +73,7 @@ public class FeedBackController {
         return new ModelAndView("list");
     }
 
-    @PostMapping("/public/add")
-    @ResponseBody
-    public RedirectView inserirFeedback(Feedback feedback, BindingResult result, Model model) {
-        if (result.hasErrors()) {
-            return new RedirectView("index");
-        }
-        repository.save(feedback);
 
-        return new RedirectView("/list");
-    }
 
-    @GetMapping("/public/feedback")
-    @ResponseBody
-    public ModelAndView requestForm(Feedback feedback, Model model) {
-        model.addAttribute("feedback", feedback);
-        return new ModelAndView("index");
-    }
+
 }
