9e23515ae8fccc70024ffdbbecacae19d7269a3b
guthemberg.alves <guthorodrigues2009@gmail.com>
2023-11-14 20:39:26 -0300

    ajustes nas configurações da DATABASE ajustes nas configurações de segurança

--- dataethics/src/main/java/br/unitins/dataethics/DataethicsApplication.java
+++ dataethics/src/main/java/br/unitins/dataethics/DataethicsApplication.java
@@ -2,8 +2,9 @@ package br.unitins.dataethics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class DataethicsApplication {
 
 	public static void main(String[] args) {
