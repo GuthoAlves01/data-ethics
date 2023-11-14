81829ee069510fa90d5e0c1dd328ba01c588beb3
guthemberg.alves <guthorodrigues2009@gmail.com>
2023-11-14 20:36:32 -0300

    configuration de segurança

--- dataethics/src/main/java/br/unitins/dataethics/security/SecurityConfiguration.java
+++ dataethics/src/main/java/br/unitins/dataethics/security/SecurityConfiguration.java
@@ -1,37 +1,49 @@
 package br.unitins.dataethics.security;
 
-//@Configuration
-//@EnableWebSecurity
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.authorization.AuthorityAuthorizationManager.hasRole;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer() {
-//        return (web) -> web.ignoring()
-//                // Spring Security should completely ignore URLs starting with /resources/
-//                .requestMatchers("/resources/**");
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests().requestMatchers("/list").hasRole("ADMIN, USER")
-//                .and()
-//                .authorizeHttpRequests().requestMatchers("/public/**").permitAll();
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user = User.withDefaultPasswordEncoder()
-//                .username("user")
-//                .password("password")
-//                .roles("USER")
-//                .build();
-//        UserDetails admin = User.withDefaultPasswordEncoder()
-//                .username("admin")
-//                .password("password")
-//                .roles("ADMIN", "USER")
-//                .build();
-//        return new InMemoryUserDetailsManager(user, admin);
-//    }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests((autorize) -> autorize
+                        .requestMatchers("/feedback-list").access(hasRole("ADMIN"))
+                        .requestMatchers("/**","/feedback/add").permitAll()
+                )
+                .formLogin((formLogin) -> formLogin.defaultSuccessUrl("/").permitAll())
+                .logout((logout) -> logout.logoutSuccessUrl("/").invalidateHttpSession(true).permitAll())
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return encoder;
+    }
 
 }
